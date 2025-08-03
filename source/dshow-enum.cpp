b6b35fc5d8fe17c8b1c7029f37a05622d65ee810
Emiliana <38952746+emilianavt@users.noreply.github.com>
2025-08-03 22:31:53 +0200

    Added back missing blacklist entries.

--- source/dshow-enum.cpp
+++ source/dshow-enum.cpp
@@ -476,7 +476,7 @@ static bool EnumDevice(const GUID &type, IMoniker *deviceInfo,
 
     // Ignore cameras that are not useful for face tracking anyways and might cause issues on new NVIDIA drivers
     if (deviceName.bstrVal && type == CLSID_VideoInputDeviceCategory)
-        if (wcsstr(deviceName.bstrVal, L"VSeeFaceCamera") != nullptr || wcsstr(deviceName.bstrVal, L"VTubeStudioCam") != nullptr || wcsstr(deviceName.bstrVal, L"VMC_Camera") != nullptr || wcsstr(deviceName.bstrVal, L"3tene Screen Capture") != nullptr || wcsstr(deviceName.bstrVal, L"Unity Video Capture") != nullptr || wcsstr(deviceName.bstrVal, L"Meta Quest") != nullptr)
+        if (wcsstr(deviceName.bstrVal, L"VSeeFaceCamera") != nullptr || wcsstr(deviceName.bstrVal, L"VTubeStudioCam") != nullptr || wcsstr(deviceName.bstrVal, L"VMC_Camera") != nullptr || wcsstr(deviceName.bstrVal, L"3tene Screen Capture") != nullptr || wcsstr(deviceName.bstrVal, L"Unity Video Capture") != nullptr || wcsstr(deviceName.bstrVal, L"Hologram Virtual Camera") != nullptr || wcsstr(deviceName.bstrVal, L"VTuber Maker Virtual Camera") != nullptr || wcsstr(deviceName.bstrVal, L"Meta Quest") != nullptr)
             return true;
 
 	propertyData->Read(L"DevicePath", &devicePath, NULL);
